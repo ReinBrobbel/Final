70e7e4334cd9235f2de4df3a5ba35ec7f05a98b2
ReinBrobbel <95082731+ReinBrobbel@users.noreply.github.com>
2022-03-15 14:51:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function dropIt() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+const dropbtn = document.getElementById("dropbtn"); 
+
+function GREEN() {
+  document.body.style.backgroundColor = "green";
+  document.getElementById("myDropdown").classList.toggle("show")
+}
+function BLUE() {
+  document.body.style.backgroundColor = "blue";
+  document.getElementById("myDropdown").classList.toggle("show")
+}
+function YELLOW() {
+  document.body.style.backgroundColor = "yellow";
+  document.getElementById("myDropdown").classList.toggle("show")
+}
+function PURPLE() {
+  document.body.style.backgroundColor = "purple";
+  document.getElementById("myDropdown").classList.toggle("show")
+}
